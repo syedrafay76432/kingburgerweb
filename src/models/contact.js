bb3be02bcc97fb9fa9c5c72050f8a267c9a6c76e
Syed Rafay <syedrafay76432@gmail.com>
2022-09-15 20:29:34 +0500

    add menu css

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,34 +1,34 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const Contact = mongoose.model('Contact',{
-    name:{
+const Contact = mongoose.model('Contact', {
+    name: {
         type: String,
         required: true
     },
-    Email:{
+    Email: {
         type: String,
         required: true,
-        validate(value){
-            if(!validator.isEmail(value)){
+        validate(value) {
+            if (!validator.isEmail(value)) {
                 // throw new Error("Invalid email")
                 alert("Email is incorrect")
 
             }
         }
     },
-    PhoneNumber:{
+    PhoneNumber: {
         type: Number,
-        required:true,
-        min:10
+        required: true,
+        min: 10
     },
-    Adress:{
+    Adress: {
         type: String,
         required: true
     },
-    Date:{
-        type:Date,
-        default:Date.now
+    Date: {
+        type: Date,
+        default: Date.now
     }
 })
 
